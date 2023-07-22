6c239f284ba95fa043b8af79fad1075ec798128a
Jeff Palmer <jeffrey.palmer@acm.org>
2023-07-22 10:17:41 -0700

    Add options to Development platform constructor

--- src/development.ts
+++ src/development.ts
@@ -5,17 +5,40 @@ import { GenArtBatchable, GenArtPlatform } from './platform.js'
 
 const alphabet = "123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ"
 
+export type DevelopmentOptions = {
+    hash?:string
+    isPreview?:boolean
+    width?:number
+}
+
 // Internal use only
 export class Development implements GenArtPlatform, GenArtBatchable {
     private _hash: string
     private _seed: Seed
+    private _isPreview:boolean
+    private _width:number
 
-    constructor() {
+    constructor(options:DevelopmentOptions = {}) {
         // Try to load a hash from the url
         let params = new URLSearchParams(location.search)
-        this._hash = params.get('hash') ?? Development.generateHash()
-        this._seed = Development.generateSeed(this._hash)
-        console.log(`Initializing development platform with hash: ${this._hash}`)
+
+        const { hash, isPreview, width } = {
+            // Try to load a hash from the URL, if available
+            hash: params.get('hash') ?? Development.generateHash(),
+            isPreview: true,
+            // Default to the window width
+            width: window.innerWidth * (window.devicePixelRatio ?? 2),
+            ...options
+        }
+
+        console.log(options, isPreview)
+
+        this._hash = hash
+        this._seed = Development.generateSeed(hash)
+        this._width = width
+        this._isPreview = isPreview
+
+        console.log(`Initializing development platform with hash: ${hash}`)
     }
 
     hash(): string {
@@ -27,7 +50,7 @@ export class Development implements GenArtPlatform, GenArtBatchable {
     }
 
     isPreview(): boolean {
-        return true
+        return this._isPreview
     }
 
     triggerPreview(): void {
@@ -35,8 +58,7 @@ export class Development implements GenArtPlatform, GenArtBatchable {
     }
 
     width() {
-        // Default to the window width
-        return window.innerWidth * (window.devicePixelRatio ?? 2)
+        return this._width
     }
 
     regenerateHashAndSeed() {
